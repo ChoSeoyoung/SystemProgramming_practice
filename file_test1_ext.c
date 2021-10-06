26178d6b8f308d682981fc7a6b8cb86e28d4eead
Choseoyoung <seoyoung0000@naver.com>
2021-10-06 19:18:46 +0900

    Week6

---
+++ file_test1_ext.c
@@ -0,0 +1,28 @@
+/*file_text1_ext.c: read data from a file and display them, by ChoSeoyoung, seoyoung0000@naver.com*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#define MAX_BUF 16
+char fname[] = "alphabet.txt";
+
+int main(){
+    int fd, read_size, write_size;
+    char buf[MAX_BUF];
+
+    fd=open(fname, O_RDONLY);
+    if(fd<0){
+        printf("Can't open %s file with errno %d\n",fname, errno);
+        exit(-1);
+    }
+    
+    read_size = read(fd, buf, MAX_BUF);
+    if(read_size<0){
+        printf("Can't read from file %s, size = %d\n", fname, read_size);
+        exit(-1);
+    }
+
+    write_size=write(STDOUT_FILENO, buf, read_size);
+    close(fd);
+}
